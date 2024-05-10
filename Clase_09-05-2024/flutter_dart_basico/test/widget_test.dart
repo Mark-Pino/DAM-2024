e90e9d52dbdffc0c25a81b86c8a3d5927c2b568a
Mark-Pino <mark.pino@upeu.edu.pe>
2024-05-10 09:13:52 -0500

    Clase 09/05/2024

--- Clase_09-05-2024/flutter_dart_basico/test/widget_test.dart
+++ Clase_09-05-2024/flutter_dart_basico/test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_dart_basico/main.dart';
+import 'package:flutter_dart_basico/05-map.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    //await tester.pumpWidget(const MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
